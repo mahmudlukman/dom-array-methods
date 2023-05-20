5dbdd62ca4bc5554ef124d0591ab7b6b254c0700
Lukman Mahmud <mistamann49@gmail.com>
2023-05-20 14:08:26 +0100

    Calculate wealth with reduce()

--- script.js
+++ script.js
@@ -48,6 +48,15 @@ const showMillionaires = () => {
   updateDOM()
 }
 
+// Calculate Wealth
+const calculateWealth = () => {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0)
+
+  const wealthEl = document.createElement('div')
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`
+  main.appendChild(wealthEl)
+}
+
 // add new obj to data arr
 const addData = (obj) => {
   data.push(obj)
@@ -78,3 +87,4 @@ addUserBtn.addEventListener('click', getRandomUser)
 doubleBtn.addEventListener('click', doubleMoney)
 sortBtn.addEventListener('click', sortByRichest)
 showMillionaireBtn.addEventListener('click', showMillionaires)
+calculateWealthBtn.addEventListener('click', calculateWealth)
